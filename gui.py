ef803e4fcccaa862ccf75b9a521b6649abea647f
Kai <83799062+gr812b@users.noreply.github.com>
2022-01-21 22:29:58 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,8 +1,22 @@
 import PySimpleGUI as gui 
 
-gui.theme('DarkAmber')
+CustomTheme = {'BACKGROUND': '#292929',
+                'TEXT': '#fff4c9',
+                'INPUT': '#c7e78b',
+                'TEXT_INPUT': '#000000',
+                'SCROLL': '#c7e78b',
+                'BUTTON': ('white', '#709053'),
+                'PROGRESS': ('#01826B', '#D0D0D0'),
+                'BORDER': 1,
+                'SLIDER_DEPTH': 0,
+                'PROGRESS_DEPTH': 0}
 
-layout = [  [gui.Text('Some text on Row 1')],
+gui.theme_add_new('CustomTheme', CustomTheme)
+
+gui.theme('CustomTheme')
+
+layout = [  
+            [gui.Text('Some text on Row 1')],
             [gui.Text('Enter something on Row 2'), gui.InputText()],
             [gui.Button('Ok'), gui.Button('Cancel')] ]
 
